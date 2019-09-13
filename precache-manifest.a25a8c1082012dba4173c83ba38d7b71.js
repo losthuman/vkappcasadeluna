ba3d2d01995dedccfb4d695f1a1dedffd93dd501
losthuman <p-shushkov2010@yandex.ru>
2019-09-13 10:52:03 +0500

    Updates

---
+++ precache-manifest.a25a8c1082012dba4173c83ba38d7b71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/losthuman/vkappcasadeluna/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "a574e56acd4ab62d62c1",
+    "url": "/losthuman/vkappcasadeluna/static/js/runtime~main.ef777b2b.js"
+  },
+  {
+    "revision": "db877164c11029242915",
+    "url": "/losthuman/vkappcasadeluna/static/js/main.d8361cbe.chunk.js"
+  },
+  {
+    "revision": "0174e2fbd1828250e981",
+    "url": "/losthuman/vkappcasadeluna/static/js/2.fa2dd764.chunk.js"
+  },
+  {
+    "revision": "db877164c11029242915",
+    "url": "/losthuman/vkappcasadeluna/static/css/main.d2ab6170.chunk.css"
+  },
+  {
+    "revision": "0174e2fbd1828250e981",
+    "url": "/losthuman/vkappcasadeluna/static/css/2.f3a61e16.chunk.css"
+  },
+  {
+    "revision": "58ccb0b1ec54cf2107ebea14253d990b",
+    "url": "/losthuman/vkappcasadeluna/index.html"
+  }
+];
